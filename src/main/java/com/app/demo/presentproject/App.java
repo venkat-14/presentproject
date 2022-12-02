6517a493be997ee371ced166e483354d12d01b61
venkat-14 <katikanenivenkatashivasai14@gmail.com>
2022-12-02 11:58:56 +0530

    demo for our team

---
+++ src/main/java/com/app/demo/presentproject/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.presentproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
